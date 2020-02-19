70b7c6f16e1fd24377f646f34c30f643ea8fe8b2
Virginia Wilcox <wilcoxva@gmail.com>
2020-02-18 21:09:06 -0500

    finished project

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+
+    useEffect(() => {
+        darkMode ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode");
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
+};
